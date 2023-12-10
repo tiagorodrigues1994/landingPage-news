4582f9411ae498e8acd94d0a89fd7dc6a17099c9
Tiago Rodrigues <tiago.rodriguesfr2@gmail.com>
2023-12-10 08:50:46 -0300

    Segunda parte do projeto concluida

--- homepage-news/src/App.jsx
+++ homepage-news/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ArticlesContainer from './components/ArticlesContainer'
 import Header from './components/Header'
 import MainArticle from './components/MainArticle'
 import NewContainer from './components/NewContainer'
@@ -11,8 +12,11 @@ function App() {
   return (
     <main className='px-4 pt-6 '>
      <Header />
+     <div className='lg:flex sm:gap-8'>
      <MainArticle />
      <NewContainer />
+     </div>
+     <ArticlesContainer />
      
     </main>
   )
--- homepage-news/src/components/Header.jsx
+++ homepage-news/src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/images/logo.svg'
 import NavBar from './NavBar'
 export default function Header() {
     return (
-        <header className="bg-red-500 flex items-center place-content-between mb-8">
+        <header className=" flex items-center place-content-between mb-8">
             <img src={logo} alt="logo" />
             <NavBar />
             
